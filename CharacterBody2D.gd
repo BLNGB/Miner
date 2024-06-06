a8dfeb32a153cc422c712214e740475746f48de5
BLNGB <jan_hildebrandt6453@t-online.de>
2024-06-06 20:13:05 +0200

    Dynamic Camera and Shop

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -2,6 +2,15 @@ extends CharacterBody2D
 const SPEED = 100.0
 
 @onready var anim = get_node("AnimationPlayer")
+func _ready():
+	var tilemap_rect = get_parent().get_node("Ground").get_node("TileMap").get_used_rect()
+	var tilemap_cell_size = get_parent().get_node("Ground").get_node("TileMap").tile_set.tile_size
+	$Camera2D.limit_left = (tilemap_rect.position.x * tilemap_cell_size.x) -100
+	$Camera2D.limit_right = (tilemap_rect.end.x * tilemap_cell_size.x) +100
+	$Camera2D.limit_top = (tilemap_rect.position.y * tilemap_cell_size.y) -100
+	$Camera2D.limit_bottom = (tilemap_rect.end.y * tilemap_cell_size.y) +100
+	pass
+	
 
 func _physics_process(delta):
 	var directionX = Input.get_axis("ui_left", "ui_right")
@@ -31,5 +40,4 @@ func _physics_process(delta):
 		velocity.y = move_toward(velocity.y, 0, SPEED)
 		if velocity.x == 0:
 			anim.play("Idle")
-
 	move_and_slide()
