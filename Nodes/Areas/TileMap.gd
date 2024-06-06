ae9fcfb5c4c17117c4d82fd407521a5555d71ce6
DefinitelyNTHere <bdachtler@mailbox.org>
2024-06-07 00:29:38 +0200

    cave1 changes

--- Nodes/Areas/TileMap.gd
+++ Nodes/Areas/TileMap.gd
@@ -3,7 +3,6 @@ extends TileMap
 var width = 32
 var height = 32
 @onready var player= get_parent().get_parent().get_child(1)
-@onready var tilemap = $TileMap
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
@@ -18,5 +17,5 @@ func generate(position):
 	var tile_pos = local_to_map(position)
 	for x in range(width):
 		for y in range(height):
-			tilemap.set_cell(0,Vector2(x,y),1,1)
-			tilemap.set_cell(0,Vector2i(tile_pos.x-width/2+x,tile_pos.y-height/2+y),0,Vector2(x,y))
+			set_cell(0,Vector2i(tile_pos.x-width/2+x,tile_pos.y-height/2+y),1,Vector2(0,0))
+			
