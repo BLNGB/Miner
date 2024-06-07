d2e747eff05f335a2bef890bfc3a09992b50279a
DefinitelyNTHere <bdachtler@mailbox.org>
2024-06-07 03:29:03 +0200

    ores temp fertig ist nicht richtig scaliert plus andere caves fehlen

---
+++ Nodes/Objekts/Ores/CommonOre.gd
@@ -0,0 +1,26 @@
+extends Sprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_tree_entered():
+	
+		var scene = get_tree().current_scene.name
+			
+		match scene:
+			"Cave1": texture = load("res://Sprites/Objekts/Ores/CommonOrs/CommonOreC1.png")
+			"Cave2": texture = load("res://Sprites/Objekts/Ores/CommonOrs/CommonOreC2.png")
+			"Cave3": texture = load("res://Sprites/Objekts/Ores/CommonOrs/CommonOreC3.png")
+			"Cave4": texture = load("res://Sprites/Objekts/Ores/CommonOrs/CommonOreC4.png")
+			"Cave5": texture = load("res://Sprites/Objekts/Ores/CommonOrs/CommonOreC5.png")
+			
+
+			
+	
