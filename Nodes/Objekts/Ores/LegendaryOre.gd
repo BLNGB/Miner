d1e5dd5026881b5c3a42ae457339ee669dc0eb85
BLNGB <jan_hildebrandt6453@t-online.de>
2024-06-07 17:56:45 +0200

    Added Ore func

--- Nodes/Objekts/Ores/LegendaryOre.gd
+++ Nodes/Objekts/Ores/LegendaryOre.gd
@@ -1,26 +1,40 @@
-extends Sprite2D
+extends StaticBody2D
+
+@onready var InDmgArea = false
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed("ui_attack") and $ProgressBar.visible == true and InDmgArea == true:
+		$ProgressBar.value = $ProgressBar.value - Global.StrengthLvl
+	if $ProgressBar.value != 100: 
+		$ProgressBar.visible = true
+	if $ProgressBar.value == 0:
+		Global.LegendaryOreCount = Global.LegendaryOreCount + 1
+		queue_free()
 
-func _on_tree_entered():
-	
-		var scene = get_tree().current_scene.name
-			
-		match scene:
-			"Cave1": texture = load("res://Sprites/Objekts/Ores/LegendaryOrs/LegendaryOreC1.png")
-			"Cave2": texture = load("res://Sprites/Objekts/Ores/LegendaryOrs/LegendaryOreC2.png")
-			"Cave3": texture = load("res://Sprites/Objekts/Ores/LegendaryOrs/LegendaryOreC3.png")
-			"Cave4": texture = load("res://Sprites/Objekts/Ores/LegendaryOrs/LegendaryOreC4.png")
-			"Cave5": texture = load("res://Sprites/Objekts/Ores/LegendaryOrs/LegendaryOreC5.png")
-			
+func _on_area_2d_mouse_entered():
+	$ProgressBar.visible = true
 
-			
-	
+
+func _on_area_2d_mouse_exited():
+	$ProgressBar.visible = false
+
+
+func _on_area_2d_body_entered(body):
+	if body.name == "Player":
+		InDmgArea = true
+
+
+func _on_area_2d_body_exited(body):
+	if body.name == "Player":
+		InDmgArea = false
+
+
+func _on_legendary_ore_tree_entered():
+	var scene = get_tree().current_scene.name
+	match scene:
+		"Cave1": $LegendaryOre.texture = load("res://Sprites/Objekts/Ores/LegendaryOrs/LegendaryOreC1.png")
+		"Cave2": $LegendaryOre.texture = load("res://Sprites/Objekts/Ores/LegendaryOrs/LegendaryOreC2.png")
+		"Cave3": $LegendaryOre.texture = load("res://Sprites/Objekts/Ores/LegendaryOrs/LegendaryOreC3.png")
+		"Cave4": $LegendaryOre.texture = load("res://Sprites/Objekts/Ores/LegendaryOrs/LegendaryOreC4.png")
+		"Cave5": $LegendaryOre.texture = load("res://Sprites/Objekts/Ores/LegendaryOrs/LegendaryOreC5.png")
