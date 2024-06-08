52f02e9e3f9e3d68f0b15ab8aa4dc4b2cc4b804d
BLNGB <jan_hildebrandt6453@t-online.de>
2024-06-08 23:05:10 +0200

    (Fixxed) Life Bars

--- Nodes/Objekts/Ores/LegendaryOre.gd
+++ Nodes/Objekts/Ores/LegendaryOre.gd
@@ -24,10 +24,25 @@ func _on_legendary_ore_tree_entered():
 	var scene = get_tree().current_scene.name
 	match scene:
 		"Cave1": $LegendaryOre.texture = load("res://Sprites/Objekts/Ores/LegendaryOrs/LegendaryOreC1.png")
+		"Cave1": $TextureProgressBar.texture_over = load("res://Sprites/Ui/HP Bars/C1BarTop.png")
+		"Cave1": $TextureProgressBar.texture_under = load("res://Sprites/Ui/HP Bars/C1BarBottom.png")
+		"Cave1": $TextureProgressBar.texture_progress = load("res://Sprites/Ui/HP Bars/C1Bar.png")
 		"Cave2": $LegendaryOre.texture = load("res://Sprites/Objekts/Ores/LegendaryOrs/LegendaryOreC2.png")
+		"Cave2": $TextureProgressBar.texture_over = load("res://Sprites/Ui/HP Bars/C2BarTop.png")
+		"Cave2": $TextureProgressBar.texture_under = load("res://Sprites/Ui/HP Bars/C2BarBottom.png")
+		"Cave2": $TextureProgressBar.texture_progress = load("res://Sprites/Ui/HP Bars/C2Bar.png")
 		"Cave3": $LegendaryOre.texture = load("res://Sprites/Objekts/Ores/LegendaryOrs/LegendaryOreC3.png")
+		"Cave3": $TextureProgressBar.texture_over = load("res://Sprites/Ui/HP Bars/C3BarTop.png")
+		"Cave3": $TextureProgressBar.texture_under = load("res://Sprites/Ui/HP Bars/C3BarBottom.png")
+		"Cave3": $TextureProgressBar.texture_progress = load("res://Sprites/Ui/HP Bars/C3Bar.png")
 		"Cave4": $LegendaryOre.texture = load("res://Sprites/Objekts/Ores/LegendaryOrs/LegendaryOreC4.png")
+		"Cave4": $TextureProgressBar.texture_over = load("res://Sprites/Ui/HP Bars/C4BarTop.png")
+		"Cave4": $TextureProgressBar.texture_under = load("res://Sprites/Ui/HP Bars/C4BarBottom.png")
+		"Cave4": $TextureProgressBar.texture_progress = load("res://Sprites/Ui/HP Bars/C4Bar.png")
 		"Cave5": $LegendaryOre.texture = load("res://Sprites/Objekts/Ores/LegendaryOrs/LegendaryOreC5.png")
+		"Cave5": $TextureProgressBar.texture_over = load("res://Sprites/Ui/HP Bars/C5BarTop.png")
+		"Cave5": $TextureProgressBar.texture_under = load("res://Sprites/Ui/HP Bars/C5BarBottom.png")
+		"Cave5": $TextureProgressBar.texture_progress = load("res://Sprites/Ui/HP Bars/C5Bar.png")
 
 func _on_hurt_box_area_entered(area):
 	if area.name == "Pickaxe":
