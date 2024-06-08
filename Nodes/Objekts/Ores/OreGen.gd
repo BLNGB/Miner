05382f4ed5cfe7c0b23abcf2d791a405c0930efe
DefinitelyNTHere <bdachtler@mailbox.org>
2024-06-08 20:21:53 +0200

    ore generation changes

--- Nodes/Objekts/Ores/OreGen.gd
+++ Nodes/Objekts/Ores/OreGen.gd
@@ -47,55 +47,72 @@ func _ready():
 		Global.EpicOreChance = 0.6
 		Global.LegendaryOreChance = 0.3
 	
+	var commonOreCount = 0
+	var uncommonOreCount = 0
+	var rareOreCount = 0
+	var epicOreCount = 0
+	var legendaryOreCount = 0
+	
+	var commonOre = randi_range(Global.CommonOreChance*100,Global.CommonOreChance*5000)
+	var uncommonOre = randi_range(Global.UncommonOreChance*100,Global.UncommonOreChance*5000)
+	var rareOre = randi_range(Global.RareOreChance*100,Global.RareOreChance*5000)
+	var epicOre = randi_range(Global.EpicOreChance*100,Global.EpicOreChance*5000)
+	var legendaryOre = randi_range(Global.LegendaryOreChance*100,Global.LegendaryOreChance*5000)
+	
 
 	for i in range(1000):
-		print(randi() % Global.CommonOreChance*1000)	
-	
-	
-	
-	
-	for i in range(1):
-		var CaveStairs = preload("res://Nodes/Objekts/Stairs/CaveStairs.tscn").instantiate()
-
-		randNum(minX,maxX,minY,maxY)
-		CaveStairs.global_position = Vector2(rand_x, rand_y)
-		add_child(CaveStairs)
-
-	for i in range(1000*Global.CommonOreChance):
-		var CommonOre = preload("res://Nodes/Objekts/Ores/CommonOre.tscn").instantiate()
-
-		randNum(minX,maxX,minY,maxY)
-		CommonOre.global_position = Vector2(rand_x, rand_y)
-		add_child(CommonOre)
-
-	for i in range(1000*Global.UncommonOreChance):
-		var UncommonOre = preload("res://Nodes/Objekts/Ores/UncommonOre.tscn").instantiate()
-
-		randNum(minX,maxX,minY,maxY)
-		UncommonOre.global_position = Vector2(rand_x, rand_y)
-		add_child(UncommonOre)
-
-	for i in range(1000*Global.RareOreChance):
-		var RareOre = preload("res://Nodes/Objekts/Ores/RareOre.tscn").instantiate()
-
-		randNum(minX,maxX,minY,maxY)
-		RareOre.global_position = Vector2(rand_x, rand_y)
-		add_child(RareOre)
-
-	for i in range(1000*Global.EpicOreChance):
-		var EpicOre = preload("res://Nodes/Objekts/Ores/EpicOre.tscn").instantiate()
-
-		randNum(minX,maxX,minY,maxY)
-		EpicOre.global_position = Vector2(rand_x, rand_y)
-		add_child(EpicOre)
-
-	for i in range(1000*Global.LegendaryOreChance):
-		var LegendaryOre = preload("res://Nodes/Objekts/Ores/LegendaryOre.tscn").instantiate()
-
-		randNum(minX,maxX,minY,maxY)
-		LegendaryOre.global_position = Vector2(rand_x, rand_y)
-		add_child(LegendaryOre)
 		
+		if i == 0:
+			var CaveStairs = preload("res://Nodes/Objekts/Stairs/CaveStairs.tscn").instantiate()
+			randNum(minX,maxX,minY,maxY)
+			CaveStairs.global_position = Vector2(rand_x, rand_y)
+			add_child(CaveStairs)
+			
+		if commonOre != commonOreCount:
+			commonOreCount+=1
+			var CommonOre = preload("res://Nodes/Objekts/Ores/CommonOre.tscn").instantiate()
+	
+			randNum(minX,maxX,minY,maxY)
+			CommonOre.global_position = Vector2(rand_x, rand_y)
+			add_child(CommonOre)
+			
+		if uncommonOre != uncommonOreCount:
+			uncommonOreCount+=1
+			var UncommonOre = preload("res://Nodes/Objekts/Ores/UncommonOre.tscn").instantiate()
+
+			randNum(minX,maxX,minY,maxY)
+			UncommonOre.global_position = Vector2(rand_x, rand_y)
+			add_child(UncommonOre)
+			
+		if rareOre != rareOreCount:
+			rareOreCount+=1
+			var RareOre = preload("res://Nodes/Objekts/Ores/RareOre.tscn").instantiate()
+
+			randNum(minX,maxX,minY,maxY)
+			RareOre.global_position = Vector2(rand_x, rand_y)
+			add_child(RareOre)
+		
+		if epicOre != epicOreCount:
+			epicOreCount+=1
+			var EpicOre = preload("res://Nodes/Objekts/Ores/EpicOre.tscn").instantiate()
+
+			randNum(minX,maxX,minY,maxY)
+			EpicOre.global_position = Vector2(rand_x, rand_y)
+			add_child(EpicOre)
+		
+		if legendaryOre != legendaryOreCount:
+			legendaryOreCount+=1
+			var LegendaryOre = preload("res://Nodes/Objekts/Ores/LegendaryOre.tscn").instantiate()
+
+			randNum(minX,maxX,minY,maxY)
+			LegendaryOre.global_position = Vector2(rand_x, rand_y)
+			add_child(LegendaryOre)
+		
+	print("CommonOre "+str(commonOreCount))
+	print("UncommonOre "+str(uncommonOreCount))
+	print("RareOre "+str(rareOreCount))
+	print("EpicOre "+str(epicOreCount))
+	print("LegendaryOre "+str(legendaryOreCount))
 		
 func randNum(minX,maxX,minY,maxY):
 	rand_x = randf_range(minX,maxX)
