eb17aee56fce7b6aa989444705e1280763182fb0
DefinitelyNTHere <bdachtler@mailbox.org>
2024-06-08 21:33:25 +0200

    hp change

--- Nodes/Ground/CaveGroundGen.gd
+++ Nodes/Ground/CaveGroundGen.gd
@@ -7,9 +7,33 @@ var minY = -1
 var maxY =  750/2
 
 func _ready():
+	var scene = get_tree().current_scene.name
 	
 	var tile_pos = local_to_map(position)
-	for x in range(minX,maxX):
-		for y in range(minY,maxY):
-			set_cell(0,Vector2i(x,y),0,Vector2(0,0))
+	
+	
+	if scene == "Cave1":
+		for x in range(minX,maxX):
+			for y in range(minY,maxY):
+				set_cell(0,Vector2i(x,y),1,Vector2(0,0))
+				
+	if scene == "Cave2":
+		for x in range(minX,maxX):
+			for y in range(minY,maxY):
+				set_cell(0,Vector2i(x,y),1,Vector2(1,0))
+				
+	if scene == "Cave3":
+		for x in range(minX,maxX):
+			for y in range(minY,maxY):
+				set_cell(0,Vector2i(x,y),1,Vector2(2,0))
+				
+	if scene == "Cave4":
+		for x in range(minX,maxX):
+			for y in range(minY,maxY):
+				set_cell(0,Vector2i(x,y),1,Vector2(3,0))
+				
+	if scene == "Cave5":
+		for x in range(minX,maxX):
+			for y in range(minY,maxY):
+				set_cell(0,Vector2i(x,y),1,Vector2(0,1))
 			
