a7e7cdc008fea9a1616522be58fd53620ab255f0
DefinitelyNTHere <bdachtler@mailbox.org>
2024-06-10 13:52:25 +0200

    cave update

--- Nodes/Objekts/Stairs/CaveStairs.gd
+++ Nodes/Objekts/Stairs/CaveStairs.gd
@@ -9,6 +9,8 @@ func _on_area_2d_body_entered(body: PhysicsBody2D):
 	
 	if body.name == "Player":
 		print("Player detectet")
+		
 		currentCave = get_tree().current_scene.name
 		var strArr = currentCave.split("e")
+		
 		get_tree().change_scene_to_file("res://Nodes/Areas/Cave_"+strArr[1]+".tscn")
